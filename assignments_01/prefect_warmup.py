45c5ebd60513e333aa04e1311250fc1c57dde9f2
SeJay134 <patrushev.s.job@gmail.com>
2026-04-01 16:55:49 -0400

    added prefect_warmup create_series

---
+++ assignments_01/prefect_warmup.py
@@ -0,0 +1,11 @@
+from prefect import flow, task
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+arr = np.array([12.0, 15.0, np.nan, 14.0, 10.0, np.nan, 18.0, 14.0, 16.0, 22.0, np.nan, 13.0])
+
+@task(log_prints=True)
+def create_series(arr):
+    s = pd.Series(arr, name="values")
+    return s
