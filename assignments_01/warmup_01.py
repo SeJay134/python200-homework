73a843d5eec9e002e344fb43f8a4eb3f1f94690b
SeJay134 <patrushev.s.job@gmail.com>
2026-04-01 15:37:27 -0400

    added pandas q2

--- assignments_01/warmup_01.py
+++ assignments_01/warmup_01.py
@@ -22,3 +22,36 @@ print()
 
 print(df['grade'])
 print()
+
+# Pandas Question 2
+
+for i in df['grade']:
+    if  i > 80:
+        print(i) # 85 90 95
+print()
+
+for i in df:
+    print(i)
+
+print()
+
+for col in df.columns:
+    for value in df[col]:
+        print(value) # print all values
+print()
+
+for col in df.columns:
+    for value in df[col]:
+        if isinstance(value, int) and value > 80:
+            print(value)
+print() # 85 90 95
+print()
+
+print(df['grade'].values) # [85 72 90 68 95]
+print()
+
+print(df[df['grade'] > 80])
+print()
+
+print(df['grade'][df['grade'] > 80])
+print()
