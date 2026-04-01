8ef5d98c0210a6b9b4abc11f29f2ebe35f4f9903
SeJay134 <patrushev.s.job@gmail.com>
2026-04-01 17:13:52 -0400

    added Part2 mini-project task read_csv_file

---
+++ assignments_01/project_01.py
@@ -0,0 +1,39 @@
+from prefect import flow, task, get_run_logger
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+import seaborn as sns
+from pathlib import Path
+import re
+
+
+@task(log_prints=True)
+def path_csv():
+    base = Path.cwd()
+    folder_path = base / "csv"
+    files = list(folder_path.glob("*.csv"))
+    if not files:
+        raise FileNotFoundError("was not finded csv")
+    return files
+
+@task(retries=3, retry_delay_seconds=2)
+def read_csv_file(files):
+    df = []
+    for file in files:
+        if file.suffix == ".csv":
+            try:
+                data = pd.read_csv(file, sep=";", on_bad_lines="skip")
+                year = file.stem
+                match = re.search(r"\d{4}", year)
+                if match:
+                    year = match.group()
+                    data["Year"] = int(year)
+                df.append(data)
+            except Exception as e:
+                print(f"error: {e}")
+    if not df:
+        raise ValueError("No valid CSV files were loaded")
+    
+    return pd.concat(df, ignore_index=True)
