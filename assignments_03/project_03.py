17fab04728ab7bbf4493bf4081072bc56a560bf4
SeJay134 <patrushev.s.job@gmail.com>
2026-04-15 07:23:19 -0400

    project_03 added read data

--- assignments_03/project_03.py
+++ assignments_03/project_03.py
@@ -24,3 +24,33 @@ from sklearn.metrics import (
 from sklearn.inspection import DecisionBoundaryDisplay
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+# Part 2: Mini-Project -- Spam or Ham? A Classifier Shootout
+print('Task 1: Load and Explore')
+
+# How many emails are in the dataset? How balanced are the two classes? 
+# What does that balance (or imbalance) mean for how you should interpret a raw accuracy score?
+
+# What do you notice? 
+# Are the differences between classes dramatic or subtle?
+
+# What does this heavy skew toward zero tell you about the data? Why does the numeric scale 
+# vary so dramatically across features (some are tiny fractions, others reach into the thousands)? 
+# Why might that matter for some of the models you are about to build?
+
+path = 'resources/spambase.data'
+path_col_names = 'resources/spambase.names'
+
+try:
+    if os.path.exists(path):
+        df = pd.read_csv(path, header=None)
+        
+        print(df.head()) # col 58
+    else:
+        print(f"File does not exist: {path}")
+except FileNotFoundError as e:
+    print(f"File not found: {e}")
+except Exception as e:
+    print(f"Unexpected error: {e}")
+
+print(df.shape) # (4601, 58)
