d7e6629e31eda8df2ded21560e02b8c37daf934e
SeJay134 <patrushev.s.job@gmail.com>
2026-04-15 06:59:24 -0400

    warmup_03 Preprocessing Question 1 is done

--- assignments_03/warmup_03.py
+++ assignments_03/warmup_03.py
@@ -26,3 +26,14 @@ iris = load_iris(as_frame=True)
 X = iris.data
 y = iris.target
 
+print('Preprocessing Question 1')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+X_train_shape = X_train.shape
+print('X_train.shape', X_train_shape)
+print('y_train.shape', y_train.shape)
+X_test_shape = X_test.shape
+print('X_test.shape', X_test_shape)
+print('y_test.shape', y_test.shape)
+print()
+
