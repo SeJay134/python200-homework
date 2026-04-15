00bdfcf1adb90becf0d39464e6b9a5b436fc98fc
SeJay134 <patrushev.s.job@gmail.com>
2026-04-15 06:57:03 -0400

    added libraries

---
+++ assignments_03/warmup_03.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+from sklearn.datasets import load_iris, load_digits # scikit-learn
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    precision_score,
+    recall_score,
+    f1_score,
+    ConfusionMatrixDisplay
+)
+from sklearn.inspection import DecisionBoundaryDisplay
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
