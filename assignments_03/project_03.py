33856f972e81943c5bf86dc119e246753670c89e
SeJay134 <patrushev.s.job@gmail.com>
2026-04-15 07:30:09 -0400

    project_03 Task 2: Prepare Your Data added train

--- assignments_03/project_03.py
+++ assignments_03/project_03.py
@@ -130,3 +130,19 @@ plt.show()
 # If features are not normalized, large-scale features (like capital run length) may dominate smaller ones.
 # Therefore, feature scaling is important before training models.
 
+print('Task 2: Prepare Your Data')
+
+X = df.drop("spam", axis=1)
+y = df["spam"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+X_train_shape = X_train.shape
+print('X_train.shape', X_train_shape)
+print('y_train.shape', y_train.shape)
+X_test_shape = X_test.shape
+print('X_test.shape', X_test_shape)
+print('y_test.shape', y_test.shape)
+print()
